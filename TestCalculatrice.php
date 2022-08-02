928b625a069e23a22d5334e7095b7c6d41903150
tanguy.bonneau1 <tanguy.bonneau@livecampus.tech>
2022-08-02 16:39:09 +0200

    final commit

--- TestCalculatrice.php
+++ TestCalculatrice.php
@@ -2,9 +2,29 @@
 use PHPUnit\Framework\TestCase;
 
 public class TestCalculatrice extends TestCase {
-    puclic function testAddition(){
-        $this->assertEquals(
-            new calculator
-        );
+    public function testAddition(){
+        $calc = new MyCalculator(1,1);
+        $result = $calc->Addition();
+        $expected = 2;
+        $this->assertSame($expected,$result);
     }
+    public function testSubtraction(){
+        $calc = new MyCalculator(4,2);
+        $result = $calc->Subtraction();
+        $expected = 2;
+        $this->assertSame($expected,$result);
+    }
+    public function testMultiplication(){
+        $calc = new MyCalculator(2,2);
+        $result = $calc->Multiplication();
+        $expected = 4;
+        $this->assertSame($expected,$result);
+    }
+    public function testDivision(){
+        $calc = new MyCalculator(2,2);
+        $result = $calc->Multiplication();
+        $expected = 4;
+        $this->assertSame($expected,$result);
+    }
+
 }
